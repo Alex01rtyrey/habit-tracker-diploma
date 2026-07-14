1e4fab27c61d7a27f91d6027d4f8048c7fe23322
Alex01rtyrey <Martinz2003@yandex.ru>
2026-07-14 16:07:18 +0300

    Добавил Layout и общий Header для приложения

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+
+function MainLayout() {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    )
+}
+
+export default MainLayout;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function HomePage () {
     return (
         <div>
-            <h1>Habit Tracker</h1>
+            <h1>Добро пожаловать в приложение трекинга привычек</h1>
             <Button text="Добавить привычку"/>
             <Link to="/login">Войти</Link>
             <Link to="/register">Регистрация</Link>
