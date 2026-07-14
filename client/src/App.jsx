380bf9a9c22dc8d6d62c7013550bf0acd35c74aa
Alex01rtyrey <Martinz2003@yandex.ru>
2026-07-14 15:22:23 +0300

    Настроил маршрутизацию через React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
