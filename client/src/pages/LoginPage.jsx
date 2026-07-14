4c1991ba3757b73395f79f8d9e17f75dbdbfd1ac
Alex01rtyrey <Martinz2003@yandex.ru>
2026-07-14 17:08:15 +0300

    Добавил формы входа и регистрации

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 
 
 function LoginPage () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(email);
+        console.log(password);
+    }
+
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <h1>Страница входа</h1>
+            
+            <label>Email:</label>
+            <input
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <label>Пароль:</label>
+            <input
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+
+            <button type="submit">
+                Войти
+            </button>
+        </form>
     )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,37 @@
-
+import { useState } from "react";
 
 function RegisterPage () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log(email);
+        console.log(password);
+    }
+
     return (
-        <div>
-            <h1>Register Page</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <h1>Страница регистрации</h1>
+
+            <label>Email:</label>
+            <input
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+
+            <label>Пароль:</label>
+            <input
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+            />
+            <button type="submit">
+                Регистрация
+            </button>
+        </form>
     )
 }
 
