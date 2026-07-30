32398a326b4d461b84b3271ec2a48f78d28441fd
Alex01rtyrey <Martinz2003@yandex.ru>
2026-07-30 17:38:38 +0300

    Создал API слой для работы с привычками

--- client/src/api/habitApi.js
+++ client/src/api/habitApi.js
@@ -3,23 +3,72 @@ export const getHabits = () => {
     return fetch('/api/habits', {
         method: "GET"
     })
-    .then(response => response.json())
+    .then(response => {
+        if(!response.ok ) {
+            throw new Error("Сервер вернул ошибку");
+        } 
+        return response.json();    
+    })
 };
 
 export const getHabitById = (id) => {
+    return fetch(`/api/habits/${id}`, {
+        method: "GET"
+    })
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error('Ошибка при загрузке привычки!');
+        }
 
+        return response.json();
+    })
 };
 
 export const deleteHabit = (id) => {
-    
+    return fetch(`/api/habits/${id}` , {
+        method: "DELETE",
+    })
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error('Ошибка при удалении');
+        }
+
+        return true;
+    })
 };
 
 export const updateHabit = (id, data) => {
+    return fetch(`/api/habits/${id}`, { 
+        method: "PATCH",
+        headers:{ 
+            "Content-Type": "application/json" 
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error('Ошибка при обновлении привычки');
+        }
 
+        return response.json();
+    })
 };
 
 export const createHabit = (data) => {
+    return fetch('/api/habits', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error ("Ошибка создания привычки")
+        } 
 
+        return response.json();
+    })
 };
 
 
